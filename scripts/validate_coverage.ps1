ee9a259c363c8813442309537738565dd06b8fef
Andre Carpio <carpioandre7@gmail.com>
2025-05-01 21:20:37 -0400

    fixing script paths

--- scripts/validate_coverage.ps1
+++ scripts/validate_coverage.ps1
@@ -1,4 +1,4 @@
-$xmlFilePath = "..\myApp.Tests\reports\coverage.cobertura.xml"
+$xmlFilePath = ".\myApp.Tests\reports\coverage.cobertura.xml"
 
 # Read the XML file content
 $xmlContent = Get-Content -Path $xmlFilePath -Raw
